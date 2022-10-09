9a20f8c4bd8f09c3515cb1a44cdca1423153ab80
Aurroraa <86052173+Aurroraa@users.noreply.github.com>
2022-10-09 09:06:20 +0700

    Create permutation.cpp

---
+++ permutation.cpp
@@ -0,0 +1,7 @@
+vector<int> permutation;
+for (int i = 0; i < n; i++) {
+permutation.push_back(i);
+}
+do {
+// process permutation
+} while (next_permutation(permutation.begin(),permutation.end()));
